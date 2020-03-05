fb597a94e2bf47d4e81bcf4260e380b64fb8d6c4
Michael Leonffu <leonffu.michael@gmail.com>
2020-03-04 18:43:20 -0800

    Started real game

---
+++ game.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+struct Noun{
+}
+
+struct Scene{
+}
+
+struct Item{
+}
+
+struct Person{
+}
+
+
+
+
+
+
+int main(){
+
+
+  return 0;
+}
--- makefile
+++ makefile
@@ -3,3 +3,6 @@
 
 dialog: dialog.cpp
 	g++ -Wall -O2 -g dialog.cpp -o dialog
+
+game: game.cpp
+	g++ -Wall -O2 -g game.cpp -o game
